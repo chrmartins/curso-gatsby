88ebe6561c4989566064fe41db092c0277989705
Christian Martins <cmts2010@gmail.com>
2019-09-23 18:25:08 -0300

    Aula 10 finalizada

---
+++ src/components/Profile/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Christian Martins </h1>
+    <h2>Analista Programador</h2>
+    <p>
+      CEO da CHR3 Tecnologia, escrevo sobre o mundo do desenvolvimento web, 
+      viagens, vida pessoal e mais. 
+    </p>
+  </div>
+)
+
+export default Profile
