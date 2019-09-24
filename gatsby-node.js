980d8fee98af3b6d6d265605395e6c5fa1479852
Christian Martins <cmts2010@gmail.com>
2019-09-24 01:06:28 -0300

    Aula 39 finalizada

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,22 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
-// You can delete this file if you're not using it
+// Para adicionar o campo slug a cada postagem
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  // Garante que estamos processando apenas arquivos de remarcação
+  if (node.internal.type === "MarkdownRemark") {
+    // Use `createFilePath` para transformar arquivos de remarcação no diretório` data / faqs` em `/ faqs / slug`
+    const slug = createFilePath({
+      node,
+      getNode,
+      basePath: "pages",
+    })
+
+    // Cria um novo campo query'able com o nome 'slug'
+    createNodeField({
+      node,
+      name: "slug",
+      value: `/${slug.slice(12)}`,
+    })
+  }
+}
