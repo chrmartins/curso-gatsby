c65e9402d3b0c2c3c636693a0a4aef8650872dec
Christian Martins <cmts2010@gmail.com>
2019-09-23 22:39:14 -0300

    Aula 29 finalizada

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,24 @@
+const links = [
+    {
+      label: "Github",
+      url: 'https://github.com/chrmartins',
+    },
+    {
+      label: "Twitter",
+      url: 'https://twitter.com/chrmartins03',
+    },
+    {
+      label: "Youtube",
+      url: 'https://www.youtube.com/cmts2010',
+    },
+    {
+      label: "Instagram",
+      url: 'https://www.instagram.com/chrmartins',
+    },
+    {
+      label: "Unsplash",
+      url: 'https://unsplash.com/chrmartins',
+    },
+  ]
+  
+  export default links
