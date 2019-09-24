b3398c726982540db1c0a9d9c08f0bce53e488e5
Christian Martins <cmts2010@gmail.com>
2019-09-24 01:16:34 -0300

    Aula 40 fnalizada

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 // Para adicionar o campo slug a cada postagem
@@ -20,3 +21,33 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
     })
   }
 }
+
+// Para criar as páginas de postagens
+exports.createPages = ({ graphql, actions }) => {
+    const { createPage } = actions
+    return graphql(`
+      {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `).then(result => {
+      result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        createPage({
+          path: node.fields.slug,
+          component: path.resolve(`./src/templates/blog-post.js`),
+          context: {
+            // Os dados passados para o contexto estão disponíveis
+            // nas consultas de página como variáveis do GraphQL.
+            slug: node.fields.slug,
+          },
+        })
+      })
+    })
+  }
