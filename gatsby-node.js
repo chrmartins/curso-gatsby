99260140e0c5a7540851faf09e6fa6b517cf9762
Christian Martins <cmts2010@gmail.com>
2019-09-24 17:38:45 -0300

    Aula 46 finalizada

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,19 +1,19 @@
-const path = require('path')
+const path = require("path")
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
-// Para adicionar o campo slug a cada postagem
+// To add the slug field to each post
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
-  // Garante que estamos processando apenas arquivos de remarcação
+  // Ensures we are processing only markdown files
   if (node.internal.type === "MarkdownRemark") {
-    // Use `createFilePath` para transformar arquivos de remarcação no diretório` data / faqs` em `/ faqs / slug`
+    // Use `createFilePath` to turn markdown files in our `data/faqs` directory into `/faqs/slug`
     const slug = createFilePath({
       node,
       getNode,
       basePath: "pages",
     })
 
-    // Cria um novo campo query'able com o nome 'slug'
+    // Creates new query'able field with name of 'slug'
     createNodeField({
       node,
       name: "slug",
@@ -22,32 +22,57 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
   }
 }
 
-// Para criar as páginas de postagens
 exports.createPages = ({ graphql, actions }) => {
-    const { createPage } = actions
-    return graphql(`
-      {
-        allMarkdownRemark {
-          edges {
-            node {
-              fields {
-                slug
-              }
+  const { createPage } = actions
+  return graphql(`
+    {
+      allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+        edges {
+          node {
+            fields {
+              slug
             }
+            frontmatter {
+              background
+              category
+              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+              description
+              title
+            }
+            timeToRead
           }
         }
       }
-    `).then(result => {
-      result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-        createPage({
-          path: node.fields.slug,
-          component: path.resolve(`./src/templates/blog-post.js`),
-          context: {
-            // Os dados passados para o contexto estão disponíveis
-            // nas consultas de página como variáveis do GraphQL.
-            slug: node.fields.slug,
-          },
-        })
+    }
+  `).then(result => {
+    const posts = result.data.allMarkdownRemark.edges
+
+    posts.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          // Data passed to context is available
+          // in page queries as GraphQL variables.
+          slug: node.fields.slug,
+        },
       })
     })
-  }
+
+    const postsPerPage = 6
+    const numPages = Math.ceil(posts.length / postsPerPage)
+
+    Array.from({ length: numPages }).forEach((_, index) => {
+      createPage({
+        path: index === 0 ? `/` : `/page/${index + 1}`,
+        component: path.resolve(`./src/templates/blog-list.js`),
+        context: {
+          limit: postsPerPage,
+          skip: index * postsPerPage,
+          numPages,
+          currentPage: index + 1,
+        },
+      })
+    })
+  })
+}
