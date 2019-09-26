ce91d72c87fc477bdcb4f17a5c957d6ba15d2824
Christian Martins <cmts2010@gmail.com>
2019-09-26 16:18:42 -0300

    Aula 70 finalizada

--- src/components/MenuLinks/styled.js
+++ src/components/MenuLinks/styled.js
@@ -1,7 +1,12 @@
 import styled from "styled-components"
+import media from "styled-media-query"
 import { Link } from "gatsby"
 
-export const MenuLinksWrapper = styled.nav``
+export const MenuLinksWrapper = styled.nav`
+  ${media.lessThan("large")`
+    display: none;
+  `}
+`
 
 export const MenuLinksList = styled.ul`
   font-size: 1.2rem;
@@ -10,7 +15,6 @@ export const MenuLinksList = styled.ul`
 
 export const MenuLinksItem = styled.li`
   padding: 0.5rem 0;
-  
   .active {
     color: var(--highlight);
   }
